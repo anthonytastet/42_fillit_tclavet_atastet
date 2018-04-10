05d361ecb8cba12772f79d8901435fca6cdbb5d5
Anthony TASTET <atastet@e2r8p9.42.fr>
2018-04-10 11:39:45 +0200

    ft_strdup

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atastet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/10 10:15:05 by atastet           #+#    #+#             */
-/*   Updated: 2018/04/10 10:57:51 by atastet          ###   ########.fr       */
+/*   Created: 2018/04/10 10:55:29 by atastet           #+#    #+#             */
+/*   Updated: 2018/04/10 11:38:17 by atastet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
 
-int	ft_strlen(char *str)
+char	*ft_strdup(char *str)
 {
 	int i;
+	char *cpy;
+	int j;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
+	j = 0;
+	i = ft_strlen(str);
+	cpy = (char *)malloc(sizeof(*str) * (i + 1));
+	while (str[j])
+	{
+		cpy[j] = str[j];
+		j++;
+	}
+	cpy[j] = '\0';
+	return (cpy);
 }
