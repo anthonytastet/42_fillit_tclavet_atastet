111709eec09957fd0352526ff886cf696596819f
Anthony TASTET <atastet@e2r8p9.42.fr>
2018-04-10 16:57:00 +0200

    Makefile

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: atastet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 10:56:58 by atastet           #+#    #+#             */
-/*   Updated: 2018/04/10 16:16:41 by atastet          ###   ########.fr       */
+/*   Updated: 2018/04/10 16:56:02 by atastet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ char		*ft_strdup(char *str);
 int			ft_strcmp(char *s1, char *s2);
 char		*ft_strcpy(char *dst, const char *src);
 char		*ft_strncpy(char *dst, const char *src, size_t len);
-char		*ft_strcat(char *s1, char *s2);
-char		*ft_strncat(char *s1, char *s2, int n);
+char		*ft_strcat(char *s1, const char *s2);
+char		*ft_strncat(char *s1, const char *s2, size_t n);
+int			ft_atoi(const char *str);
