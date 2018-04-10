15b5b4241bbaca073bb7f509f6bda201478c9a96
Anthony TASTET <atastet@e2r8p9.42.fr>
2018-04-10 16:39:30 +0200

    ft_atoi.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: atastet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 10:56:58 by atastet           #+#    #+#             */
-/*   Updated: 2018/04/10 15:22:23 by atastet          ###   ########.fr       */
+/*   Updated: 2018/04/10 16:16:41 by atastet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@ char		*ft_strdup(char *str);
 int			ft_strcmp(char *s1, char *s2);
 char		*ft_strcpy(char *dst, const char *src);
 char		*ft_strncpy(char *dst, const char *src, size_t len);
-char		*strcat(char *s1, char *s2);
+char		*ft_strcat(char *s1, char *s2);
+char		*ft_strncat(char *s1, char *s2, int n);
