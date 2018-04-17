db7f199f4708e742503c667edcdd310a4a05d2e9
Anthony TASTET <atastet@e2r7p13.42.fr>
2018-04-17 17:13:56 +0200

    just before starting mem

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: atastet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 10:56:58 by atastet           #+#    #+#             */
-/*   Updated: 2018/04/17 16:34:30 by atastet          ###   ########.fr       */
+/*   Updated: 2018/04/17 17:02:49 by atastet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 size_t		ft_strlen(const char *str);
 char		*ft_strdup(const char *str);
 int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strcpy(char *dst, const char *src);
 char		*ft_strncpy(char *dst, const char *src, size_t len);
 char		*ft_strcat(char *s1, const char *s2);
