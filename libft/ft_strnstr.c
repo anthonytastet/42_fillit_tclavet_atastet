2e8006da7a19f8f2518cba86ae3d80283d1d803d
Anthony TASTET <atastet@e3r8p9.42.fr>
2018-04-24 17:14:22 +0200

    Correction strclr + strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,21 +6,32 @@
 /*   By: atastet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 13:58:37 by atastet           #+#    #+#             */
-/*   Updated: 2018/04/17 14:45:10 by atastet          ###   ########.fr       */
+/*   Updated: 2018/04/24 17:12:15 by atastet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
-	char	*cpy;
-	int		i;
+	size_t		i;
+	char		*ndl;
+	char		*hst;
+	size_t		len;
 
-	i = ft_strlen(needle);
-	if ((cpy = (char *)malloc(sizeof(*cpy) * (i + 1))) == NULL)
-		return (NULL);
-	cpy = ft_strncpy(cpy, needle, len);
-	cpy = ft_strstr(haystack, (char *)cpy);
-	return (cpy);
+	if (*needle == '\0')
+		return ((char *)haystack);
+	hst = (char *)haystack;
+	ndl = (char *)needle;
+	len = ft_strlen(ndl);
+	i = 0;
+	while (hst[i] != '\0' && (i + len) <= n)
+	{
+		if (ft_strncmp(haystack + i, needle, len) == 0)
+		{
+			return (hst + i);
+		}
+		i++;
+	}
+	return (NULL);
 }
