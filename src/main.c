f45d168a6eea6a64eca1fb48bfcc92b9b588fc0b
Anthony TASTET <atastet@e2r10p2.42.fr>
2018-05-15 18:08:02 +0200

    [v2.1.1] Norminette ok exept bug make_grid. Next step : norminette wmake_grid + leaks

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 /*   By: atastet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/08 10:37:45 by atastet           #+#    #+#             */
-/*   Updated: 2018/05/15 15:46:06 by atastet          ###   ########.fr       */
+/*   Updated: 2018/05/15 18:06:47 by atastet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "../include/fillit.h"
 
-int	main(int argc, char **argv)
+int				main(int argc, char **argv)
 {
 	char		**tab_tet;
 	t_tetrimino	*lst;
@@ -21,7 +21,7 @@ int	main(int argc, char **argv)
 	char		**grid;
 	int			i;
 
-	i = 0;
+	i = -1;
 	if (argc != 2)
 	{
 		ft_usage();
@@ -29,45 +29,13 @@ int	main(int argc, char **argv)
 	}
 	if ((tab_tet = read_tetriminos(argv[1])) == NULL)
 		return (0);
-	//int j;
-	//j = 0;
 	lst = make_lst_tetriminos(tab_tet);
 	lst_start = lst;
 	free(tab_tet);
-	//DELETE 
-	/*while (lst->next)
-	  {
-	  dprintf(1, "lst->x %d\n", lst->x);
-	  dprintf(1, "lst->y %d\n", lst->y);
-	  dprintf(1, "lst->letter %c\n", lst->letter);
-	  dprintf(1,"lst 0 %s\n" ,lst->tetrimino[0]);
-	  dprintf(1,"lst 1 %s\n" ,lst->tetrimino[1]);
-	  dprintf(1,"lst 2 %s\n" ,lst->tetrimino[2]);
-	  dprintf(1,"lst 3 %s\n" ,lst->tetrimino[3]);
-	  dprintf(1,"lst 4 %s\n\n" ,lst->tetrimino[4]);
-	  lst = lst->next;
-	  }*/
-	lst = lst_start; //MAYBE NOT USEFULL IF WE DELETE PRINT LST (Need a check)
+	lst = lst_start;
 	lst = arrange_tet(lst);
-	/*while (lst->next)
-	  {
-	  dprintf(1, "lst->x %d\n", lst->x);
-	  dprintf(1, "lst->y %d\n", lst->y);
-	  dprintf(1, "lst->letter %c\n", lst->letter);
-	  dprintf(1, "lst->width %d\n", lst->width);
-	  dprintf(1, "lst->height %d\n", lst->height);
-	  dprintf(1,"lst 0 %s\n" ,lst->tetrimino[0]);
-	  dprintf(1,"lst 1 %s\n" ,lst->tetrimino[1]);
-	  dprintf(1,"lst 2 %s\n" ,lst->tetrimino[2]);
-	  dprintf(1,"lst 3 %s\n" ,lst->tetrimino[3]);
-	  dprintf(1,"lst 4 %s\n\n" ,lst->tetrimino[4]);
-	  lst = lst->next;
-	  }*/
 	grid = make_grid(lst_start, 3);
-	while (grid[i])
-	{
+	while (grid[++i])
 		ft_putendl(grid[i]);
-		i++;
-	}
 	return (0);
 }
